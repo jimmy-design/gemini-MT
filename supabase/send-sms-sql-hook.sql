896cbfecc049527e5106031ebb6c154e1451b358
james <jamesmchaa@gmail.com>
2026-09-07 12:28:28 +0300

    Fix SQL SMS auth hook return type

--- supabase/send-sms-sql-hook.sql
+++ supabase/send-sms-sql-hook.sql
@@ -1,7 +1,9 @@
 create extension if not exists pg_net with schema extensions;
 
+drop function if exists public.send_sms(jsonb);
+
 create or replace function public.send_sms(event jsonb)
-returns void
+returns jsonb
 language plpgsql
 security definer
 set search_path = public, extensions, net
@@ -13,6 +15,8 @@ begin
     headers := '{"Content-Type": "application/json"}'::jsonb,
     timeout_milliseconds := 1000
   );
+
+  return '{}'::jsonb;
 end;
 $$;
 
