92322bb61df20efdd3af972d94b6e32c09d9b1bf
james <jamesmchaa@gmail.com>
2026-09-07 12:32:50 +0300

    Match Supabase SMS SQL hook signature

--- supabase/send-sms-sql-hook.sql
+++ supabase/send-sms-sql-hook.sql
@@ -3,7 +3,7 @@ create extension if not exists pg_net with schema extensions;
 drop function if exists public.send_sms(jsonb);
 
 create or replace function public.send_sms(event jsonb)
-returns jsonb
+returns void
 language plpgsql
 security definer
 set search_path = public, extensions, net
@@ -15,8 +15,6 @@ begin
     headers := '{"Content-Type": "application/json"}'::jsonb,
     timeout_milliseconds := 1000
   );
-
-  return '{}'::jsonb;
 end;
 $$;
 
